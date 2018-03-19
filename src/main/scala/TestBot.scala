78ab8516c29758229077ab616049dec9e774064b
Henrique Silva <henrique.p.silva@outlook.com>
2018-03-19 15:34:47 -0300

    couple of stuff

--- src/main/scala/TestBot.scala
+++ src/main/scala/TestBot.scala
@@ -1,18 +1,13 @@
 import bwapi.{Unit => ScUnit, _}
 import bwta.BWTA
 
-object TestBot {
-  def main(args: Array[String]) =
-    new TestBot().run()
-}
-
 class TestBot extends DefaultBWListener {
     val mirror = new Mirror()
     var game: Game = _
     var self: Player = _
 
     def run(): Unit = {
-        mirror.getModule().setEventListener(this)
+        mirror.getModule.setEventListener(this)
         mirror.startGame()
     }
 
@@ -33,27 +28,32 @@ class TestBot extends DefaultBWListener {
     }
 
     override def onFrame(): Unit = {
-      //game.setTextSize(10);
-      game.drawTextScreen(10, 10, "Playing as " + self.getName + " - " + self.getRace)
+        //game.setTextSize(10);
+        game.drawTextScreen(10, 10, "Playing as " + self.getName + " - " + self.getRace)
 
-      import scala.collection.JavaConverters._
+        import scala.collection.JavaConverters._
 
-      self.getUnits.asScala
-        .filter(_.getType == UnitType.Terran_Command_Center && self.minerals >= 50)
-        .foreach(_.train(UnitType.Terran_SCV))
+        self.getUnits.asScala
+            .filter(_.getType == UnitType.Terran_Command_Center && self.minerals >= 50)
+            .foreach(_.train(UnitType.Terran_SCV))
 
-      self.getUnits.asScala
-        .filter(_.getType.isWorker)
-        .filter(_.isIdle)
-        .foreach { worker =>
-        val closestMineral = game.neutral.getUnits.asScala
-          .filter(_.getType.isMineralField)
-          .map(mineral => (mineral.getDistance(worker), mineral))
-          .sortBy(_._1)
-          .map(_._2)
-          .headOption
+        self.getUnits.asScala
+            .filter(_.getType.isWorker)
+            .filter(_.isIdle)
+            .foreach { worker =>
+                val closestMineral = game.neutral.getUnits.asScala
+                    .filter(_.getType.isMineralField)
+                    .map(mineral => (mineral.getDistance(worker), mineral))
+                    .sortBy(_._1)
+                    .map(_._2)
+                    .headOption
 
-        closestMineral.foreach(worker.gather)
-      }
+                closestMineral.foreach(worker.gather)
+            }
     }
+}
+
+object TestBot {
+    def main(args: Array[String]): Unit =
+        new TestBot().run()
 }

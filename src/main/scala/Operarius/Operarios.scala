12a77cf5f70f5100eaa390da709718808f8c8fcd
Henrique Silva <henrique.p.silva@outlook.com>
2018-04-08 02:23:13 -0300

    did some studying of existing bots, I'm starting to get where and how to start

---
+++ src/main/scala/Operarius/Operarios.scala
@@ -0,0 +1,9 @@
+package Operarius
+
+/** The workers data class.
+  *
+  * It keeps track of each worker and its current job
+  */
+class Operarios {
+
+}
---
+++ src/main/scala/Operarius/VilicusOperarius.scala
@@ -0,0 +1,10 @@
+package Operarius
+
+/** The worker controller class.
+  *
+  * It manages how many workers are active, how many minerals
+  * are being gathered and any new request for new workers.
+  */
+class VilicusOperarius {
+
+}

6bc2862ab7631f4ea6be21b5fb7655ad201bbbef
Gabriel <44637g@gmail.com>
2018-06-23 01:18:20 -0300

    We train workers

--- src/main/scala/VilicusOpum.scala
+++ src/main/scala/VilicusOpum.scala
@@ -1,5 +1,7 @@
 import BroodWarUnits.BWAPIConnection
 import bwapi._
+
+import scala.collection.JavaConverters._
 /** The resource controller class.
   *
   * It manages how many resources are there, how many buildings
@@ -9,5 +11,14 @@ class VilicusOpum(gameCons: Game, selfCons: Player)  extends BWAPIConnection{
   //connect(gameCons,selfCons) //this will run on instantiation
   def getUnits() ={
     print("Resources\n")
+    val allUnits = self.getUnits.asScala
+    val gameUnits = game.neutral.getUnits.asScala
+    for(i <- allUnits){
+      if(i.getType==UnitType.Terran_Command_Center){
+        print("moar workers\n")
+        i.train(UnitType.Terran_SCV)
+      }
+    }
+
   }
 }
